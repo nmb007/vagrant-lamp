7dbd9c055e96f4d20656b5211ac4c94550371a14
Naveed Masaud Butt <naveed.butt@metadesign.eu>
2015-10-01 16:57:03 +0500

    Change files

--- puphpet/files/exec-once/custom.sh
+++ puphpet/files/exec-once/custom.sh
@@ -2,7 +2,7 @@
 echo "#LoadModule dummy_module /usr/lib/apache2/modules/mod_dummy.so" >> /etc/httpd/conf/httpd.conf
 
 # Compile and Install mod_xsendfile module in apache
-sudo apxs -cia /var/www/conrad/puphpet/files/dot/mod_xsendfile.c
+sudo apxs -cia /var/www/project/puphpet/files/dot/mod_xsendfile.c
 
 # Replacing the incorrect line added by the previous command with correct one
 sudo sed -i '47 c\ LoadModule xsendfile_module   /usr/lib64/httpd/modules/mod_xsendfile.so' /etc/httpd/conf/httpd.conf
@@ -11,7 +11,7 @@ sudo sed -i '47 c\ LoadModule xsendfile_module   /usr/lib64/httpd/modules/mod_xs
 sudo /sbin/service httpd restart
 
 # Replace prefilled placeholder '## XSEND' in Virtual Host with the following statement
-sudo sed -i "s/## XSEND/XSendFile On \\r\\n XSendFilePath \/var\/www\/conrad\/files/" /etc/httpd/conf.d/25-av_nvjebw8jor9g.conf
+sudo sed -i "s/## XSEND/XSendFile On \\r\\n XSendFilePath \/var\/www\/project\/files/" /etc/httpd/conf.d/25-av_nvjebw8jor9g.conf
 
 # Restart Apache
 sudo /sbin/service httpd restart
