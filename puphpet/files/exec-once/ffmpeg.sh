76ca17e3270cbd93c71581f0a6d6e0fc9bc20390
Naveed Masaud Butt <naveed.butt@metadesign.eu>
2015-10-16 13:12:21 +0500

    Modify ffmpeg shell script

--- puphpet/files/exec-once/ffmpeg.sh
+++ puphpet/files/exec-once/ffmpeg.sh
@@ -1,149 +1,138 @@
 #!/bin/sh
-# 150326 | 15:53:00, michael.haehnel@metadesign.de
 
-sudo yum -y erase ffmpeg x264 x264-devel
-sudo yum -y install gcc make nasm pkgconfig wget speex-devel
+sudo yum erase ffmpeg faac libfaac x264 libx264 libvpx
 
-sudo mkdir -p /usr/local/dist/
-#svn update # NOT NEEDED
+sudo yum install gcc gcc-c++ automake autoconf libtool git subversion pkgconfig
 
-sudo mkdir -p /usr/local/ffmpeg-source
+export LD_LIBRARY_PATH=/usr/local/lib/
+sudo echo /usr/local/lib > sudo /etc/ld.so.conf.d/custom-libs.conf
+sudo ldconfig
 
-PREFIX="/usr/local/ffmpeg-source"
-BIN="/usr/local/bin"
+sudo wget http://downloads.xvid.org/downloads/xvidcore-1.3.2.tar.gz
+sudo tar zxf xvidcore-1.3.2.tar.gz
+cd xvidcore/build/generic
+sudo ./configure --enable-shared
+sudo make 
+sudo make install
+sudo ldconfig
+cd ../../../
 
-cd /usr/local/ffmpeg-source
+sudo wget http://downloads.sourceforge.net/project/lame/lame/3.99/lame-3.99.5.tar.gz
+sudo tar zxf lame-3.99.5.tar.gz
+cd lame-3.99.5
+sudo ./configure --enable-shared
+sudo make
+sudo make install
+sudo ldconfig
+cd ..
 
-# YASM
-cd /usr/local/ffmpeg-source
-sudo curl -O http://www.tortall.net/projects/yasm/releases/yasm-1.2.0.tar.gz
+sudo wget http://downloads.sourceforge.net/faac/faac-1.28.tar.gz
+sudo tar xzf faac-1.28.tar.gz
+cd faac-1.28
+sudo ./bootstrap
+sudo ./configure --enable-shared
+sudo make
+sudo make install
+sudo ldconfig
+cd ..
+
+sudo wget http://downloads.sourceforge.net/project/opencore-amr/opencore-amr/opencore-amr-0.1.3.tar.gz
+sudo tar xzf opencore-amr-0.1.3.tar.gz
+cd opencore-amr-0.1.3
+sudo ./configure --enable-shared
+sudo make 
+sudo make install
+sudo ldconfig
+cd ..
+
+sudo wget http://downloads.xiph.org/releases/ogg/libogg-1.3.0.tar.gz
+sudo tar xzf libogg-1.3.0.tar.gz
+cd libogg-1.3.0
+sudo ./configure --enable-shared
+sudo make
+sudo make install
+sudo ldconfig
+cd ..
+
+sudo wget http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.3.tar.gz
+sudo tar xzf libvorbis-1.3.3.tar.gz
+cd libvorbis-1.3.3
+sudo ./configure --enable-shared
+sudo make 
+sudo make install
+sudo ldconfig
+cd ..
+
+sudo wget http://downloads.xiph.org/releases/theora/libtheora-1.1.1.tar.bz2
+sudo tar xjf libtheora-1.1.1.tar.bz2
+cd libtheora-1.1.1
+sudo ./configure --enable-shared
+sudo make
+sudo make install
+sudo ldconfig
+cd ..
+
+# Yasm is an assembler used by x264 and FFmpeg.
+sudo wget http://www.tortall.net/projects/yasm/releases/yasm-1.2.0.tar.gz
 sudo tar xzvf yasm-1.2.0.tar.gz
 cd yasm-1.2.0
-sudo ./configure --prefix="/usr/local/ffmpeg-source" --bindir="/usr/local/bin"
-sudo make -j12
-sudo make install
-sudo make distclean
-
-
-# X264
-cd /usr/local/ffmpeg-source
-sudo git clone --depth 1 git://git.videolan.org/x264
-cd x264
-sudo ./configure --prefix="/usr/local/ffmpeg-source" --bindir="/usr/local/bin" --disable-asm
-sudo make -j12 && sudo make install
-sudo make distclean
-
-#AAC audio encoder.
-cd /usr/local/ffmpeg-source
-sudo git clone --depth 1 git://git.code.sf.net/p/opencore-amr/fdk-aac
-cd fdk-aac
-sudo autoreconf -fiv
-sudo ./configure --prefix="/usr/local/ffmpeg-source" --disable-shared
-sudo make -j12
-sudo make install
-sudo make distclean
-
-
-# LAME
-cd /usr/local/ffmpeg-source
-sudo wget http://downloads.sourceforge.net/project/lame/lame/3.99/lame-3.99.5.tar.gz
-sudo tar xzf lame-3.99.5.tar.gz
-cd lame-3.99.5
-sudo ./configure --prefix="/usr/local/ffmpeg-source" --bindir="/usr/local/bin" --disable-shared --enable-nasm
-sudo make -j12 && sudo make install
-
-
-# Opus
-cd /usr/local/ffmpeg-source
-sudo curl -O http://downloads.xiph.org/releases/opus/opus-1.0.3.tar.gz
-sudo tar xzvf opus-1.0.3.tar.gz
-cd opus-1.0.3
-sudo ./configure --prefix="/usr/local/ffmpeg-source" --disable-shared
-sudo make -j12
-sudo make install
-sudo make distclean
-
-
-# libogg
-cd /usr/local/ffmpeg-source
-sudo curl -O http://downloads.xiph.org/releases/ogg/libogg-1.3.1.tar.gz
-sudo tar xzvf libogg-1.3.1.tar.gz
-cd libogg-1.3.1
-sudo ./configure --prefix="/usr/local/ffmpeg-source" --disable-shared
-sudo make -j12
-sudo make install
-sudo make distclean
-
-# libtheora
-cd /usr/local/ffmpeg-source
-sudo curl -O http://downloads.xiph.org/releases/theora/libtheora-1.1.1.tar.gz
-sudo tar xzvf libtheora-1.1.1.tar.gz
-cd libtheora-1.1.1
-sudo ./configure --prefix="/usr/local/ffmpeg-source" --with-ogg="/usr/local/ffmpeg-source" --disable-examples --disable-shared --disable-sdltest --disable-vorbistest
-sudo make -j12
-sudo make install
-sudo make distclean
-
-# libvorbis
-cd /usr/local/ffmpeg-source
-sudo curl -O http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.3.tar.gz
-sudo tar xzvf libvorbis-1.3.3.tar.gz
-cd libvorbis-1.3.3
-sudo ./configure --prefix="/usr/local/ffmpeg-source" --with-ogg="/usr/local/ffmpeg-source" --disable-shared
-sudo make -j12
-sudo make install
-sudo make distclean
-
-# vo-aacenc
-cd /usr/local/ffmpeg-source
-sudo wget http://downloads.sourceforge.net/opencore-amr/vo-aacenc-0.1.2.tar.gz
-sudo tar xzf vo-aacenc-0.1.2.tar.gz
-cd vo-aacenc-0.1.2
-sudo ./configure --disable-shared
-sudo make -j12 && sudo make install
-
-# libvpx
-cd /usr/local/ffmpeg-source
-sudo git clone --depth 1 https://chromium.googlesource.com/webm/libvpx.git
-cd libvpx
-sudo ./configure --prefix="/usr/local/ffmpeg-source" --disable-examples
-sudo make -j12
-sudo make install
-sudo make clean
-
-
-# zlib
-cd /usr/local/ffmpeg-source
-sudo wget http://zlib.net/zlib-1.2.8.tar.gz
-sudo tar xzf zlib-1.2.8.tar.gz
-cd zlib-1.2.8
 sudo ./configure
-sudo make -j12 && sudo make install
-
-# speex
-cd /usr/local/ffmpeg-source
-sudo wget http://downloads.xiph.org/releases/speex/speex-1.2rc1.tar.gz
-sudo gzip -dc speex-1.2rc1.tar.gz | sudo tar -xf -
-
-
-# FFmpeg itself
-cd /usr/local/ffmpeg-source
-sudo git clone --depth 1 git://source.ffmpeg.org/ffmpeg
-cd ffmpeg
-PKG_CONFIG_PATH="/usr/local/ffmpeg-source/lib/pkgconfig"
-export PKG_CONFIG_PATH
-sudo ./configure --prefix="/usr/local/ffmpeg-source" --extra-cflags="-I/usr/local/ffmpeg-source/include" --extra-ldflags="-L/usr/local/ffmpeg-source/lib" --bindir="/usr/local/bin" --extra-libs="-ldl" --enable-gpl --enable-nonfree --enable-libfdk_aac --enable-libmp3lame --enable-libvorbis --enable-libspeex --enable-libfreetype --enable-version3 --enable-libtheora --enable-libvo-aacenc --disable-yasm
-sudo make -j12
+sudo make
 sudo make install
-sudo make distclean
-hash -r
+sudo ldconfig
+cd ..
 
+sudo git clone git://git.videolan.org/x264.git
+cd x264
+sudo ./configure --enable-static --enable-shared
 
+# Fix the error in between that will cause problems in installing x264
+PATH=$PATH:/usr/local/bin sudo ./configure --enable-shared
+PATH=$PATH:/usr/local/bin sudo make && sudo make install
 
+sudo make
+sudo make install
+sudo ldconfig
+cd ..
 
-#Compilation Guide RHEL6 (used for RHEL5)
-#http://ffmpeg.org/trac/ffmpeg/wiki/CentosCompilationGuide
+sudo wget http://downloads.sourceforge.net/opencore-amr/vo-aacenc-0.1.2.tar.gz
+sudo tar xzvf vo-aacenc-0.1.2.tar.gz
+cd vo-aacenc-0.1.2
+sudo ./configure --enable-shared
+sudo make 
+sudo make install
+sudo ldconfig
+cd ..
 
+#NASM
+sudo wget www.nasm.us/pub/nasm/releasebuilds/2.11.08/nasm-2.11.08.tar.xz
+sudo tar -xf nasm-2.11.08.tar.xz
+cd nasm-2.11.08
+sudo ./configure --prefix=/usr
+sudo make
+sudo make install
+cd ..
 
-# larger setup
-## ./configure --enable-gpl --enable-libmp3lame --enable-libtheora --enable-libvo-aacenc --enable-libvorbis --enable-libvpx --enable-libx264 --enable-version3 --enable-pthreads --enable-libfaac --enable-libxvid --enable-nonfree   --disable-ffserver --disable-mmx  
+sudo git clone https://chromium.googlesource.com/webm/libvpx.git 
+cd libvpx
+sudo ./configure --enable-shared
+sudo make 
+sudo make install
+sudo ldconfig
+cd ..
+
+sudo wget http://zlib.net/zlib-1.2.8.tar.gz
+sudo tar xzvf zlib-1.2.8.tar.gz
+cd zlib-1.2.8
+sudo ./configure --enable-shared
+sudo make
+sudo make install
+sudo ldconfig
+cd ..
+
+sudo git clone git://git.videolan.org/ffmpeg.git ffmpeg
+cd ffmpeg
+sudo ./configure --enable-version3 --enable-libopencore-amrnb --enable-libopencore-amrwb --enable-libvpx --enable-libfaac --enable-libmp3lame --enable-libtheora --enable-libvorbis --enable-libx264 --enable-libxvid --enable-gpl --enable-postproc --enable-nonfree --enable-pthreads --enable-libvo-aacenc --enable-postproc --enable-swscale --enable-avfilter --enable-shared --enable-runtime-cpudetect --disable-stripping --extra-cflags='-O2 -g -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions -fstack-protector --param=ssp-buffer-size=4 -m64 -mtune=generic -fPIC'
+sudo make
+sudo make install
+sudo ldconfig
